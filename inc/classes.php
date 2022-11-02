850a8ca0ef3a42206ccc0bdd54110709fda0bd44
Serhii Chyzhykov <serhii.chyzhykov@md.p2h.com>
2022-11-02 13:45:54 +0200

    Update theme

--- inc/classes.php
+++ inc/classes.php
@@ -89,10 +89,10 @@ class Header_Walker_Nav_Menu extends Walker_Nav_Menu {
 		$item_output .= '<a' . $attributes . '>';
 		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
 
-		if ( $depth == 0 ) {
-			$item_output .= '<i class="material-icons">' . get_field( 'icon', $item->ID ) . '</i>';
+		if ( $icon = get_field( 'icon', $item->ID ) ) {
+			$item_output .= '<i class="material-icons">' . $icon . '</i>';
 		}
-		
+
 		$item_output .= '</a>';
 		$item_output .= $args->after;
 
